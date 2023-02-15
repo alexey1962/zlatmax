acb04ebe19ef8a2e3c24776605eda618d350a409
Alexey <a.kashutin.dev@gmail.com>
2023-02-15 22:52:57 +0600

    remove dynamicAdaptive function

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import * as flsFunctions from "./modules/functions.js"
 flsFunctions.isWebp()
 flsFunctions.spollers()
 flsFunctions.initPopups()
-flsFunctions.dynamicAdapt()
 flsFunctions.swiper()
 flsFunctions.rating()
 
