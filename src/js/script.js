18e3c41b3f64a08aac16dcd8e26a84bf63965562
vsnegovik <snegovik.dev@gmail.com>
2023-02-21 21:54:20 +0300

    fix: toggle menu item

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,9 @@ function documentActions(e) {
 
             if(activeLink && activeLink !== targetElement) {
               activeLink.classList.remove('_sub-menu-active')
-              activeBlock.classList.remove('_sub-menu-open')
+            document.querySelectorAll('._sub-menu-open').forEach(el => {
+                el.classList.remove('_sub-menu-open')
+            })
               document.documentElement.classList.remove('_sub-menu-open')
             }
             document.documentElement.classList.toggle('_sub-menu-open')
