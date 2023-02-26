9b3fa98979532ed58af7cabbd4c91c69418799d5
Alexey <a.kashutin.dev@gmail.com>
2023-02-26 17:54:31 +0600

    add filter for catalog page

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -608,22 +608,28 @@ export function burgerMenu() {
 
 export function rangeInit() {
   const rangeItems = document.querySelectorAll('[data-range]')
+  
   if(rangeItems.length) {
     rangeItems.forEach(rangeItem => {
-      const fromValue = rangeItem.querySelector('data-range-from')
-      const toValue = rangeItem.querySelector('data-range-to')
+      const fromValue = rangeItem.querySelector('[data-range-from]')
+      const toValue = rangeItem.querySelector('[data-range-to]')
       const item = rangeItem.querySelector('[data-range-item]')
+      const inputs = [fromValue, toValue]
+
       noUiSlider.create(item, {
-        // start: [Number(fromValue.value), Number(toValue.value)],
-        start: [0, 9999],
+        start: [Number(fromValue.value), Number(toValue.value)],
         connect: true,
+        tooltips: [true, true],
+        step: 500,
         range: {
-          // 'min': [Number(fromValue.dataset.rangeFrom)],
-          // 'max': [Number(toValue.dataset.rangeTo)]
-          'min': [0],
-          'max': [9999]
+        'min': [Number(fromValue.dataset.rangeFrom)],
+        'max': [Number(toValue.dataset.rangeTo)]
         }
       })
+
+      item.noUiSlider.on('update', function (values, handle) {
+        inputs[handle].value = values[handle];
+      });
     })
   }
 }
